2024143fe4660d04b8265cfcfdb40d2e578609c3
Erik Pukinskis <erik@snowedin.net>
2022-07-29 15:10:40 -0600

    Add some text fields

--- src/Survey.tsx
+++ src/Survey.tsx
@@ -2,18 +2,21 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
 
 export function Survey() {
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+      <Box component="form" sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Koneksa Survey
         </Typography>
-        <Typography variant="body1" component="p">
-          Hello, wordle
-        </Typography>
+
+        <FormGroup>
+          <TextField required label="Name" />
+          <TextField required label="Password" type="password" />
+        </FormGroup>
       </Box>
     </Container>
   );
