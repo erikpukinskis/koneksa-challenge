4bc5a0a52318dedbb385b88efd47bc613bcfdf6a
Erik Pukinskis <erik@snowedin.net>
2022-08-01 12:46:44 -0600

    Add tests for unselecting toppings and submitting name and password

--- src/Survey.test.tsx
+++ src/Survey.test.tsx
@@ -26,18 +26,61 @@ describe("Survey", () => {
     requestBody = undefined;
   });
 
-  it("can choose toppings", async () => {
-    const { getByLabelText, getByRole } = render(<Survey />);
+  const submit = async () => {
+    const submit = await screen.getByRole("button", { name: "Submit" });
+    await userEvent.click(submit);
+    await waitFor(() => expect(requestBody).toBeDefined());
+  };
+
+  it("submits toppings", async () => {
+    const { getByLabelText } = render(<Survey />);
 
     const artichoke = await getByLabelText("Artichoke Hearts");
     await userEvent.click(artichoke);
-    const submit = await getByRole("button", { name: "Submit" });
-    await userEvent.click(submit);
-    await waitFor(() => expect(requestBody).toBeDefined());
+
+    await submit();
 
     expect(requestBody).toHaveProperty(
       "pizzaToppings",
       expect.arrayContaining(["Artichoke Hearts"])
     );
+  });
+
+  it("unselects toppings", async () => {
+    const { getByLabelText } = render(<Survey />);
+
+    const hot = await getByLabelText("Italian sausage (Hot)");
+    const sweet = await getByLabelText("Italian sausage (Sweet)");
+
+    await userEvent.click(hot);
+    await userEvent.click(sweet);
+    await userEvent.click(hot);
+
+    await submit();
+
+    expect(requestBody).toHaveProperty(
+      "pizzaToppings",
+      expect.arrayContaining(["Italian sausage (Sweet)"])
+    );
+
+    expect(requestBody).toHaveProperty(
+      "pizzaToppings",
+      expect.not.arrayContaining(["Italian sausage (Hot)"])
+    );
+  });
+
+  it("submits name and password", async () => {
+    const { getByLabelText } = render(<Survey />);
+
+    const name = await getByLabelText("Name");
+    await userEvent.type(name, "Erik P");
+    const password = await getByLabelText("Password");
+    await userEvent.type(password, "pass0rdw-");
+
+    await submit();
+
+    expect(requestBody).toHaveProperty("name", "Erik P");
+
+    expect(requestBody).toHaveProperty("password", "pass0rdw-");
   });
 });
